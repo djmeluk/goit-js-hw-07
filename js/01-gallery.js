bc432c0273aacd0febf43a898f3f2579ce4b8ede
Oleh Tomashevskiy <kulemjd@ukr.net>
2023-01-05 22:07:12 +0200

    1 task completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const cardMarkup = createPictureMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", cardMarkup);
+galleryList.addEventListener("click", onClickGalleryImg);
+
+function createPictureMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+
+function onClickGalleryImg(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+
+  instance.show();
+}
