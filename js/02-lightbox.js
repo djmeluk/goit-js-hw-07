8cbbdda6e8aef9e7e4cab906510ae1a24e9836c5
Oleh Tomashevskiy <kulemjd@ukr.net>
2023-01-06 09:38:55 +0200

    task 02 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const cardMarkup = createPictureMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", cardMarkup);
+galleryList.addEventListener("click", onClickGalleryImg);
+
+function createPictureMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+
+function onClickGalleryImg(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+
+    }
+}
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+});
