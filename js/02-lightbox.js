0dbb466951740bf57d5403089cdc917636eac07a
Oleh Tomashevskiy <kulemjd@ukr.net>
2023-01-06 11:31:40 +0200

    refactoring task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,26 +4,16 @@ const galleryList = document.querySelector(".gallery");
 const cardMarkup = createPictureMarkup(galleryItems);
 
 galleryList.insertAdjacentHTML("beforeend", cardMarkup);
-galleryList.addEventListener("click", onClickGalleryImg);
 
 function createPictureMarkup(galleryItems) {
-  return galleryItems
+   
+    return galleryItems
     .map(({ preview, original, description }) => {
       return `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`;
     })
     .join("");
-}
-
-
-function onClickGalleryImg(event) {
-    event.preventDefault();
-
-    if (!event.target.classList.contains("gallery__image")) {
-        return;
-
-    }
 }
 const gallery = new SimpleLightbox('.gallery a', {
     captions: true,
